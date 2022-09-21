b3c20f06cb0e95c0740cb7c74444c40e013c064f
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-21 21:34:57 +0000

    fetch data api

--- src/context.jsx
+++ src/context.jsx
@@ -2,20 +2,18 @@ import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext()
 
-
 const AppProvider = ({ children }) => {
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('https://randomuser.me/api/')
-        const data = await response.json()
+        const data = await response.json();
         console.log(data)
       } catch (error) {
         console.log(error)
       }
     }
-    
+    fetchData()
   }, [])
 
   return <AppContext.Provider value={{ name: 'temiloluwa', role: 'student' }}>
