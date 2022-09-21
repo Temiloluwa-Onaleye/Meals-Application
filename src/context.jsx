d47abd24be4dd6a937e68ce451efab6ec990d5f5
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-21 21:39:14 +0000

    external function

--- src/context.jsx
+++ src/context.jsx
@@ -2,17 +2,24 @@ import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext()
 
+
+
+
+
 const AppProvider = ({ children }) => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://randomuser.me/api/')
-        const data = await response.json();
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-      }
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api/')
+      const data = await response.json();
+      console.log(data)
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  useEffect(() => {
+
     fetchData()
   }, [])
 
