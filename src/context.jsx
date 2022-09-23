7e969832059015cc63f8d25b5996a610a0f8e5ef
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-23 21:27:02 +0000

    meals db random meals api

--- src/context.jsx
+++ src/context.jsx
@@ -4,7 +4,7 @@ const AppContext = React.createContext()
 
 const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
 
-const randomMealUrl = ''
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 
 
