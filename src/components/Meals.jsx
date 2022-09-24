28da336b3f5d4b297637e50360291c01adcefd3a
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-24 08:55:55 +0000

    setup loading

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -5,34 +5,37 @@ const Meals = () => {
 
   const { meals, loading } = useGlobalContext();
 
-  {
-    loading === true ? <section className='section'>
-      <h4>Loading...</h4>
-    </section> : false
+
+  if (loading) {
+    return (
+      <section className='section'>
+        <h4>Loading...</h4>
+      </section>)
+
   }
+
   return (
-    <section className="section-center">
-      {loading}
-      {
-        meals.map((meal) => {
-          const { idMeal, strMeal: title, strMealThumb: image } = meal
-          return (
-            <article className="single-meal" key={idMeal} id={idMeal}>
-              <img src={image} alt="image" className="img" />
-              <footer>
-                <h5>{title}</h5>
-                <button className='like-btn'>
-                  <BiLike />
-                </button>
-              </footer>
-            </article>
-          )
-        })
+    <section className="section-center">{
+      meals.map((meal) => {
+        const { idMeal, strMeal: title, strMealThumb: image } = meal
+        return (
+          <article className="single-meal" key={idMeal} id={idMeal}>
+            <img src={image} alt="image" className="img" />
+            <footer>
+              <h5>{title}</h5>
+              <button className='like-btn'>
+                <BiLike />
+              </button>
+            </footer>
+          </article>
+        )
+      })
 
 
-      }
+    }
     </section>
   )
+
 
 
 
