7f58ed6ed7ad0cb3329542d322f840647223985c
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-24 10:04:49 +0000

    setting up the structure for the search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,20 @@
+import React, {useState} from 'react';
+import {useGlobalContext} from '../context'
+
+
 const Search = () => {
 
   return(
 
-    <div>
-    <h1>Who are you</h1>
-    </div>
+    
+    <header className="search-container">
+    <form className="">
+    <input className="form-input" type="text" placeholder="type favourite meal" />
+      <button className=".btn" type="submit">Search</button>
+      <button className="btn btn-hipster" type="btn">Surprise me!</button>
+    </form>
+    </header>
+  
   )
     
   
