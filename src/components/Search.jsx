7ac4036e6b2d5aefa046dcd53dfb7e393807138f
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-25 17:57:28 +0000

    setting up Text state variable

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,34 @@
-import React, {useState} from 'react';
-import {useGlobalContext} from '../context'
+import React, { useState } from 'react';
+import { useGlobalContext } from '../context'
 
 
 const Search = () => {
+  const [text, setText] = useState('')
 
-  return(
+  const handleChange = (event) => {
+    event.preventDefault()
+    setText(event.target.value)
+    console.log(setText)
+  }
 
-    
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+
+
     <header className="search-container">
-    <form className="">
-    <input className="form-input" type="text" placeholder="type favourite meal" />
-      <button className=".btn" type="submit">Search</button>
-      <button className="btn btn-hipster" type="btn">Surprise me!</button>
-    </form>
+      <form className="">
+        <input className="form-input" type="text" placeholder="type favourite meal" onChange={handleChange} />
+        <button onSubmit={handleSubmit} className="btn" type="submit">Search</button>
+        <button className="btn btn-hipster" type="button">Surprise me!</button>
+      </form>
     </header>
-  
+
   )
-    
-  
+
+
 }
 
 export default Search;
