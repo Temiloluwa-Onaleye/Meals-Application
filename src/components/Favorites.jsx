bf5abea9452bfa491656e5faf48b7d42865783a7
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-09-27 07:45:17 +0000

    css tweaking

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,36 +1,36 @@
-import {useGlobalContext} from '../context'
+import { useGlobalContext } from '../context'
 
 
 const Favorites = () => {
 
-const {favorites, selectMeal, removeFromFavorites} = useGlobalContext();
-  
-  return(
+  const { favorites, selectMeal, removeFromFavorites } = useGlobalContext();
+
+  return (
 
     <section className='favorites'>
       <div className='favorites-content'>
-      <h5>Favourites</h5>
+        <h5>Favourites</h5>
         <div className='favorites-container'>
           {favorites.map((item) => {
-      const {idMeal, strMealThumb: image} = item  
-      return <div key={idMeal} className="favorites-item">
-      <img src={image} className="favorites-img img"/>
-        <button className="remove-btn" onClick={()=> removeFromFavorites(idMeal)}>remove</button>
-      </div>
-      
-                                   
-                                   }
-                      
-                        )
-            
+            const { idMeal, strMealThumb: image } = item
+            return <div key={idMeal} className="favorite-item">
+              <img src={image} className="favorites-img img" />
+              <button className="remove-btn" onClick={() => removeFromFavorites(idMeal)}>remove</button>
+            </div>
+
+
+          }
+
+          )
+
           }
         </div>
       </div>
 
     </section>
   )
-    
-  
+
+
 }
 
 export default Favorites;
